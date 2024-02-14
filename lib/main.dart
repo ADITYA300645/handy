c83cbdf614091a52d774bd6f00dcd66f09312280
Sagar <jokeeblack@gmail.com>
2024-02-14 19:33:46 +0530

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:handy/pages/controllers/JWTController/JWTController.dart';
-import 'package:handy/pages/Auth/Login/LoginPage.dart';
-import 'package:handy/pages/home/HomePage.dart';
+import 'package:handy/src/pages/Auth/SignUp/SignUpPage.dart';
+import 'package:handy/src/pages/controllers/JWTController/JWTController.dart';
+import 'package:handy/src/pages/Auth/Login/LoginPage.dart';
 import 'package:handy/Theme/theme.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); //Initilize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); //Initilize Firebase
   final appDoc = await getApplicationDocumentsDirectory();
   Hive.init(appDoc.path);
   await Hive.openBox("secrets");
@@ -29,12 +31,13 @@ class _MyAppState extends State<MyApp> {
   var jwtController = Get.put(JWTController());
 
   @override
-  Widget build(BuildContext context)  {
+  Widget build(BuildContext context) {
     return GetMaterialApp(
-    //  create: (context) => UserProvider(),
+      //  create: (context) => UserProvider(),
       title: 'Handy',
       theme: lightTheme,
-      home:  Obx(() => jwtController.isAuth.value ? const HomePage() : const LoginPage()),
+      home: Obx(() =>
+          jwtController.isAuth.value ? const SignUpPage() : const LoginPage()  ),
       debugShowCheckedModeBanner: false,
     );
   }
